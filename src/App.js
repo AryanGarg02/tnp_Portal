7d6f668372bbde09a91b63355b1737602c1a6db6
AryanGarg02 <garg.aryan029@gmail.com>
2021-11-16 19:21:30 +0530

    login pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import Logo from './ensveeLogo.svg';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { useState } from 'react';
 
 function Login() {
+  const [email, setEmail] = useState("")
+  const onchangeHandler = (e) => {
+    console.log(e.target.value);
+    setEmail(e.target.value)
+  }
+
   return (
     <>
     <div className = "header">
@@ -14,43 +21,103 @@ function Login() {
       <h1>LOGIN</h1>
       <div className="LoginForm">
         <form action="loginin">
-          <label htmlFor="" className="credentials">Email ID</label>
+          <div className="emailInput">
+          <label htmlFor="email" className="credentials">Email ID</label>
           <br />
-          <input type="text" name="" id="inputArea" />
+          <input type="text" onChange={onchangeHandler} name="" id="email" value={email} className="inputbox"/>
+          </div>
+          <div className="pwdInput">
+          <label htmlFor="pwd" className="credentials">Password</label>
           <br />
-          <label htmlFor="" className="credentials">Password</label>
-          <br />
-          <input type="password" name="" id="inputArea" />
-          <br />
-          <button type="submit" className="loginBtn">Log In</button>
+          <input type="password" name="" id="pwd" className="inputbox"/>
+          </div>
+          <button type="submit" className={email != "" ? "loginbtn btnActive" : "loginBtn" }>Log In</button>
         </form>
       </div>
+    </div>
+    <div className="redirectLinks">
+      <a href="/forgotpassword" className="forgot">Forgot Password?</a>
+      <br />
+      <a href="/newsignin" className="newUser">New User? Sign in now</a>
     </div>
     </>
   );
 }
 
 function PwdChange() {
+  const [email, setEmail] = useState("")
+  const onchangeHandler = (e) => {
+    console.log(e.target.value);
+    setEmail(e.target.value)
+  }
 
   return(
+    <>
     <div className = "header">
       <div className ="ensvee">
       <img src={Logo} className="logo" alt="Ensvee" />
       </div>  
     </div>
 
+    <div className="passwordReset">
+      <h1>Forgot Password</h1>
+      <p>A password reset link will be sent to your email ID</p>
+    </div>
+    
+    <div className="resetLink">
+      <form action="password-reset">
+        <div>
+        <label htmlFor="email" className="credentials">Email ID</label>
+        <br />
+        <input type="text" onChange={onchangeHandler} name="" id="email" className = "inputbox"/>
+        </div>
+        <button type="submit" className={email != "" ? "pwdBtn pwdbtnActive" : "pwdBtn" }>Send Password Reset Link</button>
+      </form>
+    </div>
+
+    </>
   );
   
 }
 function SignIn() {
+  const [email, setEmail] = useState("")
+  const onchangeHandler = (e) => {
+    console.log(e.target.value);
+    setEmail(e.target.value)
+  }
 
   return(
+    <>
     <div className = "header">
       <div className ="ensvee">
       <img src={Logo} className="logo" alt="Ensvee" />
       </div>  
     </div>
-
+    <h1 className="signInheading">SIGNIN</h1>
+    <div className="createAccount">
+      <form action="Create-New-Account" className="newAccount">
+        <div>
+          <label htmlFor="email" className="credentials">Email ID</label>
+          <br />
+          <input type="text" onChange={onchangeHandler} name="email" id="email" className = "inputbox"/>
+        </div>
+        <div>
+        <label htmlFor="password1" className="credentials">Password</label>
+          <br />
+          <input type="password" name="password1" id="password1" className = "inputbox"/>
+        </div>
+        <div>
+        <label htmlFor="password2" className="credentials">Confirm Password</label>
+          <br />
+          <input type="password"  name="password2" id="password2" className = "inputbox"/>
+        </div>
+        <button type="submit" className={email != "" ? "createBtn createbtnActive" : "createBtn" }>Create Account</button>
+      </form>
+      <div className="loginDirect">
+      <a href="/" className="logIn">Already a user? Log in now</a>
+      </div>
+    </div>
+    </>
   );
   
 }
@@ -59,9 +126,9 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/login" exact={true} component={Login} />
-        <Route path="/forgotpassword" exact={true} component={PwdChange} />
-        <Route path="/newsignin" exact={true} component={SignIn} />
+        <Route path="/" exact={true} element={<Login />} />
+        <Route path="/forgotpassword" exact={true} element={<PwdChange />} />
+        <Route path="/newsignin" exact={true} element={<SignIn />} />
       </Routes>
 
     </Router>
